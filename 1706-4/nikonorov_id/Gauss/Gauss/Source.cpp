05bad401b00286dc47391fe1f1d639138d4fd6c0
Ilya Nikonorov <nikonorov.2012@bk.ru>
2020-05-21 15:50:38 +0300

    Внёс коррективы в соотетствии с замечаниями

---
+++ 1706-4/nikonorov_id/Gauss/Gauss/Source.cpp
@@ -0,0 +1,130 @@
+#include <iostream>
+#include <ctime>
+#include <iomanip>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <omp.h>
+#include <chrono>
+
+using namespace cv;
+using namespace std;
+#define M_PI 3.14159265358979323846
+
+int Clamp(int value, int min, int max)
+{
+	if (value < min)
+		return min;
+
+	if (value > max)
+		return max;
+
+	return value;
+}
+
+void deletekernel(double* kernel, int k) {
+
+	delete [] kernel;
+}
+
+//-------------------------------------------------------------//
+//-------------------------------------------------------------//
+//-------------------------------------------------------------//
+//-------------------------------------------------------------//
+//-------------------------------------------------------------//
+
+double* createKernel(int rows, int cols) {
+	int tmp = rows * cols;
+	double* mas;
+	mas = new double[tmp];
+	return mas;
+}
+void InitKern(double* kernel, int k, double sigma)
+{
+	int radius = (k / 2);
+	double sum = 0.0;
+	double tmp = 1 / (2 * M_PI * sigma * sigma);
+	int step = radius;
+
+	for (int i = -radius; i <= radius; i++)
+	{
+		for (int j = -radius; j <= radius; j++)
+		{
+			kernel[j + step] = tmp * exp(-(i * i + j * j) / (2 * sigma * sigma));
+			sum += kernel[j + step];
+		}
+		step += k;
+	}
+	
+	for (int i = 0; i < k * k; i++)
+	{
+		kernel[i] /= sum;
+	}
+}
+
+void Gauss(const Mat& input, Mat& output, double* kernel, int kern_size)
+{
+	int radius = int(kern_size / 2);
+	for (int i = 1; i < input.rows; i++) {
+		for (int j = 1; j < input.cols; j++) {
+			double tmp = 0.0;
+			for (int l = -radius; l <= radius; l++) {
+				for (int w = -radius; w <= radius; w++) {
+					int idX = Clamp(i + l, 0, input.rows - 1);
+					int idY = Clamp(j + w, 0, input.cols - 1);
+					tmp += double(input.at<uchar>(idX, idY)) * kernel[l + w + 2 * (l + 2)];
+				}
+			}
+			output.at<uchar>(i, j) = int(tmp);
+
+		}
+	}
+}
+
+int main(int argc, char** argv) {
+	string orig_image_path;
+	string result_image_path;
+	if (argc < 3)
+	{
+		orig_image_path += "pic.jpg";
+		result_image_path += "result.jpg";
+	}
+	else
+	{
+		orig_image_path += argv[1];
+		result_image_path += argv[2];
+	}
+
+	Mat original_image;
+	original_image = imread(orig_image_path, IMREAD_COLOR);
+
+	if (original_image.empty())
+	{
+		cout << "No image data" << endl;
+		return -1;
+	}
+	Mat gray_image;
+	Mat my_result;
+	cvtColor(original_image, gray_image, COLOR_BGR2GRAY);
+	cvtColor(original_image, my_result, COLOR_BGR2GRAY);
+
+	int k = 3;
+
+	double* kernel;
+	kernel = createKernel(k, k);
+	InitKern(kernel, k, 7);
+	auto begin = std::chrono::steady_clock::now();
+	Gauss(gray_image, my_result, kernel, k);
+	auto end = std::chrono::steady_clock::now();
+	auto elapsed_ms = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin);
+	cout << "The time is: " << elapsed_ms.count() << " ms" << endl;
+
+	imwrite(result_image_path, my_result);
+	namedWindow("my_result", WINDOW_AUTOSIZE);
+	namedWindow("Gray image", WINDOW_AUTOSIZE);
+	imshow("my_result", my_result);
+	imshow("Gray image", gray_image);
+	waitKey(0);
+	deletekernel(kernel, k);
+	return 0;
+}
